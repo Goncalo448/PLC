88d8021f493d33e58d23a3bcf2067fa51a374df4
Goncalo448 <goncalosilvabusiness@gmail.com>
2022-10-21 19:06:13 +0100

    ...

--- TP1/Problema1/problema1.py
+++ TP1/Problema1/problema1.py
@@ -1,14 +1,17 @@
 import re
 
-f = open("processos.txt", "r")
+file = open("processos.txt", "r")
 
 #(a) frequência de processos por ano
 
-for linha in f:
-    filter_by_proc = re.search(r'(?i:proc).*', linha) #já tem todos os processosn(os que existem)
-    if filter_by_proc:
-        
+filtered_by_proc = []
+for line in file:
+    match = re.search(r'(?i:proc).*', line) #já tem todos os processosn(os que existem)
 
+    if match:
+        filtered_by_proc.append(line)
 
+for linha in filtered_by_proc:
+    match = re.search(r'')
 
 
