d3c76ea3d3117850c76d357cbf4fc301d9fdb8a1
Goncalo448 <goncalosilvabusiness@gmail.com>
2022-10-22 00:51:55 +0100

    progressão

--- TP1/Problema1/problema1.py
+++ TP1/Problema1/problema1.py
@@ -5,13 +5,24 @@ file = open("processos.txt", "r")
 #(a) frequência de processos por ano
 
 filtered_by_proc = []
+dic_by_years = {}
+
 for line in file:
     match = re.search(r'(?i:proc).*', line) #já tem todos os processosn(os que existem)
 
     if match:
         filtered_by_proc.append(line)
 
-for linha in filtered_by_proc:
-    match = re.search(r'')
+for line in filtered_by_proc:
+    slices = re.split(r'[::-]', line)
+    
+    if slices[2] not in dic_by_years.keys():
+        dic_by_years[slices[2]] = []
+
+    dic_by_years[slices[2]].append(line)
+
+
+for key in sorted(dic_by_years.keys()):
+    print(str(key) + ": " + str(len(dic_by_years[key])))
 
 
