913e06ce1ba8fa351969d02260e6739fe97d3633
Goncalo448 <goncalosilvabusiness@gmail.com>
2022-10-22 22:31:48 +0100

    a) feito

--- TP1/Problema1/problema1.py
+++ TP1/Problema1/problema1.py
@@ -1,28 +1,32 @@
 import re
-
-file = open("processos.txt", "r")
+import math
 
 #(a) frequência de processos por ano
 
-filtered_by_proc = []
-dic_by_years = {}
+def freq_by_years(file):
+    dic_by_years = {}
 
-for line in file:
-    match = re.search(r'(?i:proc).*', line) #já tem todos os processosn(os que existem)
+    for line in file:
+        slices = re.split(r'[::-]', line)
+        if len(slices) > 1:
+            if slices[2] not in dic_by_years.keys():
+                dic_by_years[slices[2]] = []
 
-    if match:
-        filtered_by_proc.append(line)
+            dic_by_years[slices[2]].append(line)
 
-for line in filtered_by_proc:
-    slices = re.split(r'[::-]', line)
-    
-    if slices[2] not in dic_by_years.keys():
-        dic_by_years[slices[2]] = []
-
-    dic_by_years[slices[2]].append(line)
+    return dic_by_years
 
 
-for key in sorted(dic_by_years.keys()):
-    print(str(key) + ": " + str(len(dic_by_years[key])))
+def freq_by_centuries(file):
+    return
+
+
+file = open("processos.txt", "r")
+dic = freq_by_years(file)
+
+for key in sorted(dic.keys()):
+    print(str(key) + ": " + str(len(dic[key])))
+
+
 
 
