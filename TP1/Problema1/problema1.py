a13668ff021157bf534eb4a00ea54fe39e0fe80f
Goncalo448 <goncalosilva.business@gmail.com>
2022-10-25 14:52:27 +0100

    ...

--- TP1/Problema1/problema1.py
+++ TP1/Problema1/problema1.py
@@ -3,6 +3,9 @@ import math
 
 #(a) frequência de processos por ano
 
+#r'([A-Z][a-z]+| )+'
+#filtro que dá sempre a última palavra de uma seq de palavras
+
 def freq_by_years(file):
     dic_by_years = {}
 
@@ -17,23 +20,50 @@ def freq_by_years(file):
     return dic_by_years
 
 
+def getFirstName(person):
+    firstName = re.split(r'\s+', person)
+    return firstName[0]
+
+
+def getLastName(person):
+    lastName = re.split(r'\s+', person)
+    print(lastName)
+    return lastName[-1]
+
+
 def freq_by_centuries(file):
-    return
 
-
-def freq_by_relationship(file):
-
+    dic_by_centuries = {}
     for line in file:
-        slices = re.split(r'[:{2,4}\-{1}.{1}]', line)
-        print(slices)
-        #for s in slices:
-         #   match = re.fullmatch(r'(\w+| )', s)
+        slices = re.split(r'::', line)
+        if slices[1] not in dic_by_centuries.keys():
+            dic_by_centuries[slices[1]] = {}
+        dad = slices[3]
+        mom = slices[4]
+        dad_first_name = getFirstName(dad)
+        dad_last_name = getLastName(dad)
 
     return
+
+
+'''def freq_by_relationship(file):
+
+    dic_by_centuries = {}
+    for line in file:
+        slices = re.split(r'::', line)
+        if slices[1] not in dic_by_centuries.keys():
+            dic_by_centuries[slices[1]] = {}
+        
+        dad = slices[3]
+        mom = slices[4]
+        
+
+
+    return'''
 
 
 file = open("processos.txt", "r")
-freq_by_relationship(file)
+freq_by_centuries(file)
 #dic = freq_by_years(file)
 
 #for key in sorted(dic.keys()):
