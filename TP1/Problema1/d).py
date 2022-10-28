92648cf496b850e5f0adabcaa970a4f7a608e56e
Goncalo448 <goncalosilva.business@gmail.com>
2022-10-28 10:35:24 +0100

    d) finalizada (penso eu)

--- TP1/Problema1/d).py
+++ TP1/Problema1/d).py
@@ -1,3 +1,34 @@
 import re
+import json
+import os
 
+
+path = os.getcwd()
+
+def first_20_lines(file):
+	dic = {}
+
+	with file as myfile:
+		for x in range(20):
+			if x not in dic.keys():
+				dic[x] = None
+			dic[x] = next(myfile)
+
+	return dic
+
+
+def write_into_json_file(dic):
+	json_string = json.dumps(dic)
+
+	if os.path.isfile(path + "/output.json"):
+		with open("output.json", 'w') as outfile:
+			json.dump(json_string, outfile)
+	else:
+		json_file = open("output.json", 'x')
+		json.dump(json_string, json_file)
+
+
+file = open("processos.txt", "r")
+dic = first_20_lines(file)
+write_into_json_file(dic)
 
