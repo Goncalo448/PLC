6879207800c342b5aabea96e65c58669e402033a
Goncalo448 <goncalosilva.business@gmail.com>
2022-11-01 15:08:48 +0000

    1 d) finalizado

--- TP1/Problema1/d).py
+++ TP1/Problema1/d).py
@@ -6,29 +6,57 @@ import os
 path = os.getcwd()
 
 def first_20_lines(file):
-	dic = {}
+	output_file = open("output.json", "w")
 
 	with file as myfile:
 		for x in range(20):
-			if x not in dic.keys():
-				dic[x] = None
-			dic[x] = next(myfile)
+			line = next(myfile)
+			mom = ""
+			dad = ""
+			obs = ""
 
-	return dic
+			match_dir = re.search(r'[0-9]+', line)
+			if match_dir:
+				line = re.sub(match_dir.group(), "#", line)
 
+			match_date = re.search(r'[0-9]{4}-[0-9]{2}-[0-9]{2}', line)
+			if match_date:
+				line = re.sub(match_date.group(), "#", line)
 
-def write_into_json_file(dic):
-	json_string = json.dumps(dic)
+			match_name = re.search(r'([A-Z][a-z]+|\s)+', line)
+			if match_name:
+				line = re.sub(match_name.group(), "#", line)
 
-	if os.path.isfile(path + "/output.json"):
-		with open("output.json", 'w') as outfile:
-			json.dump(json_string, outfile)
-	else:
-		json_file = open("output.json", 'x')
-		json.dump(json_string, json_file)
+			match_dad = re.search(r':{2}([A-Z][a-z]+|\s)*:{2}', line)
+			if match_dad:
+				line = re.sub(match_dad.group(), "::#::", line)
+				dad = match_dad.group().lstrip("::")
+				dad = dad.rstrip("::")
+
+			match_mom = re.search(r':{2}([A-Z][a-z]+|\s)*:{2}', line)
+			if match_mom:
+				line = re.sub(match_mom.group(), "::#::", line)
+				mom = match_mom.group().lstrip("::")
+				mom = mom.rstrip("::")
+
+			match_obs = re.search(r':{2}(,|\.|\w|\d| )*:{2}', line)
+			if match_obs:
+				line = re.sub(match_obs.group(), "::#::", line)
+				obs = match_obs.group().lstrip("::")
+				obs = obs.rstrip("::")
+
+			dic = {}
+			dic["Pasta"] = match_dir.group()
+			dic["Data"] = match_date.group()
+			dic["Name"] = match_name.group()
+			dic["Pai"] = dad
+			dic["Mae"] = mom
+			dic["Obs"] = obs
+
+			json_object = json.dumps(dic)
+			output_file.write(json_object)
 
 
 file = open("processos.txt", "r")
-dic = first_20_lines(file)
-write_into_json_file(dic)
+first_20_lines(file)
 
